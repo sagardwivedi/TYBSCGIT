091ed077f675c4f85f519986f6c4fc62de6c9e37
sagardwivedi <Sagar.dw20@gmail.com>
2022-07-12 12:05:32 +0530

    Main

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-// write simple hello world program in java
+// simple hello world program in java
 
 public class Hello {
     public static void main(String[] args) {
--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
-// write simple java program to find the sum of n natural numbers
-
-import java.util.Scanner;
+// simple java program to find the sum of n natural numbers
 
 public class Main {
     public static void main(String[] args) {
-        try (Scanner scn = new Scanner(System.in)) {
-            System.out.println("Enter the number of natural numbers to be added: ");
-            int n = scn.nextInt();
-            int sum = 0;
-            for (int i = 1; i <= n; i++) {
-                sum = sum + i;
-            }
-            System.out.println("The sum of " + n + " natural numbers is " + sum);
+
+        int n = 10;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
         }
+        System.out.println("The sum of " + n + " natural numbers is " + sum);
+
     }
 }
